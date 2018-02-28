b69083276d6b4f811a21fe65e0df7d7805326c7a
Andi <andreas@glaser-informatik.de>
2018-02-28 11:22:57 +0100

    reorganised structure and added maven

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,9 +9,9 @@ public class Main extends Application {
     //I am a comment
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("TimeTracking.fxml"));
-        Scene scene = new Scene(root,715,800);
+        Scene scene = new Scene(root, 715, 800);
         primaryStage.setTitle("Internship Diary");
         primaryStage.setScene(scene);
         scene.getStylesheets().add(Controller.class.getResource("main.css").toExternalForm());
